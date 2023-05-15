02c07f9b32c5b1f11d2d768fa18ff3870bca1a80
Varun-Babu <itsme.varun.26@gmail.com>
2023-05-15 19:44:15 +0530

    initial commit

---
+++ src/main/java/com/nestAssessment/toolSetManagement/ToolSetManagementApplication.java
@@ -0,0 +1,13 @@
+package com.nestAssessment.toolSetManagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ToolSetManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ToolSetManagementApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/nestAssessment/toolSetManagement/ToolSetManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nestAssessment.toolSetManagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ToolSetManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
